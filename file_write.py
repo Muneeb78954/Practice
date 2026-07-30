7787157fd0ef34136a60648659fec9c0ac8206df
Muneeb Ur Rehman <f2025439020@umt.edu.pk>
2026-07-30 17:23:00 +0500

    list

--- file_write.py
+++ file_write.py
@@ -1,9 +1,23 @@
 # writing with python 
-
+# Text Files
 txt_data = "I LOVE PIZZA!!!"
 
 file_path = "outout.txt"
+try:                            # w-> overrride the data | x-> give error if already exist 
+    with open(file_path, "x") as file:
+        file.write(txt_data)
+        print(f"Text File '{file_path}' Was Created  ")
+except FileExistsError:
+    print("This file already exist")
+    
 
-with open(file_path, "w") as file:
-    file.write(txt_data)
-    print(f"Text File '{file_path}' Was Created  ")
+"""For List"""
+employees = ["Elon Musk", "Bill Gates", "Larry Page"]
+
+try:                            # w-> overrride the data | x-> give error if already exist 
+    with open(file_path, "x") as file:
+        for employee in employees:
+            file.write(employee + "\n")
+        print(f"Text File '{file_path}' Was Created  ")
+except FileExistsError:
+    print("This file already exist")
